8b2bb48be392bff018aa946548ef91853284c347
sachingour <gour.sachin.sg@gmail.com>
2023-03-02 00:05:03 +0530

    changes in porfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,37 +1,14 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import youtube from "../assets/portfolio/youtube-logo-hd-wallpaper-1280x720.jpg";
+
 
 const Portfolio = () => {
   const portfolio = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: youtube,
     },
-    {
-      id: 2,
-      src: installNode,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactParallax,
-    },
-    {
-      id: 5,
-      src: reactSmooth,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
+    
   ];
   return (
     <div
@@ -56,10 +33,11 @@ const Portfolio = () => {
                 />
                 <div className="flex items-center justify-center">
                     <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Demo
+                    <a href="https://magical-alpaca-b072d5.netlify.app" target="_blank" rel="noreferrer">Demo</a> 
                     </button>
                     <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Code
+                      <a href="https://github.com/sachingour007/youtube_clone" target="_blank" rel="noreferrer"> Code</a>
+                   
                     </button>
                 </div>
                 </div>
